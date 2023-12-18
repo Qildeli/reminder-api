e878563f814721731c42b480288ce0a7d9973c57
Qildeli <anaqildeli@gmail.com>
2023-12-19 00:35:24 +0400

    Added small users views part

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,12 +1,35 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
+from .serializers import RegisterSerializer, LoginSerializer
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+user = get_user_model()
 
 
 class RegisterAPI(APIView):
+    """
+    An endpoint to create a new user.
+    """
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+
+class LoginAPI(APIView):
+    """
+    An endpoint to authenticate existing users using their email and password.
+    """
+    serializer_class = LoginSerializer
+
     def post(self, request):
         pass
 
 
-class LoginAPI(APIView):
+class LogoutAPI(APIView):
+    """
+    An endpoint to logout users.
+    """
     def post(self, request):
         pass
