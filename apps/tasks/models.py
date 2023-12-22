46c3d1009260abffc58d51abcaf5a3a4fa213a02
qildeli <anaqildeli@gmail.com>
2023-12-22 18:08:41 +0400

    Add owner field to task model

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.utils import timezone
+
+from core import settings
+
+
+def one_week_hence():
+    return timezone.now() + timezone.timedelta(days=7)
 
 
 class Task(models.Model):
     title = models.CharField(max_length=245, blank=False, db_index=True)
     created = models.DateTimeField(auto_now_add=True)
-    complete = models.BooleanField(default=False)
+    due_date = models.DateTimeField(default=one_week_hence)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
