a5493d732399670d4ca2f41b15132e79d1996df4
qildeli <anaqildeli@gmail.com>
2023-12-22 18:09:30 +0400

    Add validate method to task serializer

--- apps/tasks/serializers.py
+++ apps/tasks/serializers.py
@@ -1,3 +1,4 @@
+from django.utils.timezone import now
 from rest_framework import serializers
 from .models import Task
 
@@ -5,4 +6,14 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = ['id', 'title', 'created', 'due_date']
+
+    def validate(self, attrs):
+        attrs = super().validate(attrs)
+
+        due_date = attrs['due_date']
+
+        if due_date < now():
+            raise serializers.ValidationError({"due_date": "Due date cannot be earlier than the creation date."})
+
+        return attrs
