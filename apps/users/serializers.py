172a6757132bf1c670cb29ad174805c2347c77e8
Qildeli <anaqildeli@gmail.com>
2024-02-29 14:58:32 +0400

    refactor: Change serializer layer for better readibility

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,22 +1,12 @@
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('username', 'email', 'password')
-        extra_kwargs = {
-            'password': {
-                'write_only': True
-            }
-        }
+        fields = ("username", "email", "password")
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
-
-
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = get_user_model()
-#         fields = ('username', 'email', 'password')
