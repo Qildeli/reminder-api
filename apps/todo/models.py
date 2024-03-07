e8791bdf0be7c15f3a8afef6d7e9b1799f5feedf
Qildeli <anaqildeli@gmail.com>
2024-03-07 16:35:59 +0400

    refactor: Change app name from tasks to todo

--- apps/todo/models.py
+++ apps/todo/models.py
@@ -13,6 +13,7 @@ class Task(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     due_date = models.DateTimeField(default=one_week_hence)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    notified = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
